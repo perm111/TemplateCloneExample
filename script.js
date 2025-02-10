ba8341b53c3c5e1d5347c6acac9089737e3e6e15
Igor G <genchikmakher@gmail.com>
2025-02-09 19:13:35 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const container = document.querySelector(".container");
+const songsContainer = container.querySelector(".songs-container");
+const addButton = container.querySelector(".input__btn_action_add");
+const resetButton = container.querySelector(".input__btn_action_reset");
+const noSongsElement = container.querySelector(".no-songs");
+
+function renderHasSongs() {
+  resetButton.removeAttribute("disabled");
+  resetButton.classList.remove("input__btn_disabled");
+  noSongsElement.classList.add("no-songs_hidden");
+}
+
+function renderNoSongs() {
+  resetButton.setAttribute("disabled", true);
+  resetButton.classList.add("input__btn_disabled");
+  noSongsElement.classList.remove("no-songs_hidden");
+}
+
+function addSong(artistValue, titleValue) {
+  const songTemplate = document.querySelector("#song-template").content;
+  const songElement = songTemplate.querySelector('.song').cloneNode(true);
+
+  songElement.querySelector(".song__artist").textContent = artistValue;
+  songElement.querySelector(".song__title").textContent = titleValue;
+  songsContainer.append(songElement);
+}
+
+addButton.addEventListener("click", function () {
+  const artist = document.querySelector(".input__text_type_artist");
+  const title = document.querySelector(".input__text_type_title");
+
+  addSong(artist.value, title.value);
+  renderHasSongs();
+
+  artist.value = "";
+  title.value = "";
+});
+
+resetButton.addEventListener("click", function () {
+  const songs = document.querySelectorAll(".song")
+
+  songs.forEach((item) => {
+    item.remove();
+  });
+
+  renderNoSongs();
+});
